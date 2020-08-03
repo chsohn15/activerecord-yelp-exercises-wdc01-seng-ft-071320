21da8a1a5452b8f995c95fc0a5c107b36ed88a8a
chsohn15 <chsohn15@gmail.com>
2020-08-03 14:31:19 -0400

    Update

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -14,6 +14,21 @@ class Restaurant < ActiveRecord::Base
     end
 
     def self.max_dishes
-        
+        # Find the restaurant with the most dishes
+        max = 0
+        max_restaurant = nil
+        Restaurant.all.each do |restaurant|
+            if restaurant.dishes.count > max
+                max_restaurant = restaurant
+                max = restaurant.dishes.count
+            end
+        end
+        max_restaurant
+    end
+
+    def self.focused
+        Restaurant.all.select do |restaurant|
+           restaurant.dishes.count < 5
+        end
     end
 end
